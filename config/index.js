b29ccd69ed25e3a8190e9a14d92491bd3d3ff483
Sharang Gupta <sharanggupta96@gmail.com>
2021-04-10 18:44:53 +0100

    changed production URL for vercel deployment

--- config/index.js
+++ config/index.js
@@ -1,2 +1,2 @@
 const dev = process.env.NODE_ENV !== "production";
-export const server = dev ? "http://localhost:3000" : "https://yourwebsite.com";
+export const server = dev ? "http://localhost:3000" : "https://next-news-blog.vercel.app/";
